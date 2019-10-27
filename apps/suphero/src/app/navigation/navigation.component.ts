2a73dd9ad2fbfb9bc2abaf03cf52e6dc6763961b
Wolfram Sokollek <wolframsokollek@gmail.com>
2019-10-27 13:49:38 +0100

    added navigation to app

---
+++ apps/suphero/src/app/navigation/navigation.component.ts
@@ -0,0 +1,28 @@
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Component} from '@angular/core';
+import {RouterQuery} from '@datorama/akita-ng-router-store';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {routeNamedLinks} from '../app-routing.module';
+
+@Component({
+  selector: 'sup-hero-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.less']
+})
+export class NavigationComponent {
+  routeTitle$: Observable<string>;
+  isHandset$: Observable<boolean>;
+  routeLinks: { path: string; title: string }[];
+
+  constructor(breakpointObserver: BreakpointObserver, akitaRouterQuery: RouterQuery) {
+    this.isHandset$ =
+      breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+          map(result => result.matches),
+          shareReplay()
+        );
+    this.routeTitle$ = akitaRouterQuery.selectData('title');
+    this.routeLinks = routeNamedLinks;
+  }
+}
